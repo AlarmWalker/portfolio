23b4c978e98fe7154c17d456617e0cf7a665a287
Patrick <kkw7120@gmail.com>
2024-05-17 18:30:45 -0600

    gulp error fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ const sass = require('gulp-dart-sass');
 const wait = require('gulp-wait');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
+const del = require('del');
 
-// Define the 'scripts' task
+// Clean the 'dist' directory
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+// Build scripts into 'dist/js'
 gulp.task('scripts', function() {
     console.log('Running scripts task...');
     return gulp.src('./js/scripts.js')
@@ -17,7 +23,7 @@ gulp.task('scripts', function() {
             }
         }))
         .pipe(babel({
-          presets: [['@babel/env', { modules: false }]]
+            presets: [['@babel/env', { modules: false }]]
         }))
         .pipe(uglify({
             output: {
@@ -25,32 +31,32 @@ gulp.task('scripts', function() {
             }
         }))
         .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest('./js'));
+        .pipe(gulp.dest('dist/js'));
 });
 
-// Define the 'styles' task
+// Build styles into 'dist/css'
 gulp.task('styles', function() {
     console.log('Running styles task...');
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
-// Define the 'build' task that runs both 'scripts' and 'styles'
-gulp.task('build', gulp.series('scripts', 'styles'));
+// Copy other static files to 'dist'
+gulp.task('copy', function() {
+    return gulp.src(['./**/*', '!./scss/**/*', '!./js/**/*', '!./node_modules/**/*', '!./gulpfile.js', '!./package.json', '!./package-lock.json'])
+        .pipe(gulp.dest('dist'));
+});
 
-// Define the 'watch' task
+// Build task that runs clean, scripts, styles, and copy tasks
+gulp.task('build', gulp.series('clean', 'scripts', 'styles', 'copy'));
+
+// Watch task
 gulp.task('watch', function() {
     console.log('Starting watch task...');
-    gulp.watch('./js/scripts.js', gulp.series('scripts'))
-        .on('change', function(path, stats) {
-            console.log(`File ${path} was changed`);
-        });
-    gulp.watch('./scss/styles.scss', gulp.series('styles'))
-        .on('change', function(path, stats) {
-            console.log(`File ${path} was changed`);
-        });
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
 });
 
 // Default task
