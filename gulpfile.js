a3fd7af32db1780ced36fc257c8923de80697918
Patrick <kkw7120@gmail.com>
2024-05-24 12:46:50 -0600

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,20 @@ var plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 
+// Scripts task
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
+        .pipe(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
-        })))
+        }))
         .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
+          presets: [['@babel/env', {modules: false}]]
         }))
         .pipe(uglify({
             output: {
@@ -26,6 +27,7 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./js'));
 });
 
+// Styles task
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
@@ -33,7 +35,11 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./css'));
 });
 
+// Watch task
 gulp.task('watch', function() {
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
     gulp.watch('./scss/styles.scss', gulp.series('styles'));
 });
+
+// Build task
+gulp.task('build', gulp.series('scripts', 'styles'));
